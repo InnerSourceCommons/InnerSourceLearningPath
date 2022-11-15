f4775a9cf711cd4b538fad1f28fcc2201892d622
Laura Little <laura.nathanson@gmail.com>
2022-11-15 14:10:00 -0500

    use throttling plugin in get_contributors queries

--- scripts/get_contributors.js
+++ scripts/get_contributors.js
@@ -1,10 +1,26 @@
 require('dotenv').config()
 const { Octokit } = require("@octokit/core")
+const { throttling } = require("@octokit/plugin-throttling")
 
 module.exports = async function (filepath) {
   const unixStyleFilepath = filepath.replace(/\\/g, "/")
-  const octokit = new Octokit({
-    auth: process.env.TOKEN
+  const EnhancedOctokit = Octokit.plugin(throttling)
+  const octokit = new EnhancedOctokit({
+    auth: process.env.TOKEN,
+    throttle: {
+      onRateLimit: (retryAfter, options, octokit, retryCount) => {
+        if (retryCount <= 1) {
+          octokit.log.info(`Request quota reached for ${options.method} ${options.url}, retrying after ${retryAfter} seconds`)
+          return true
+        }
+        octokit.log.warn(`Request quota exhausted for ${options.method} ${options.url}`)
+        return false
+      },
+      onSecondaryRateLimit: (retryAfter, options, octokit) => {
+        // does not retry, only logs a warning
+        octokit.log.warn(`SecondaryRateLimit hit for request ${options.method} ${options.url}`)
+      },
+    },
   })
   const { repository: { object: { history } } } = await octokit.graphql(
     `query ($filepath: String!) {
@@ -50,7 +66,7 @@ module.exports = async function (filepath) {
 
   if (history.totalCount > 100) {
     // Needs addressing properly, but deferring to avoid merge conflicts with https://github.com/InnerSourceCommons/InnerSourceLearningPath/pull/380
-    console.warn('This script needs updating to handle >100 commits')
+    console.warn('This script needs updating to handle >100 commits: ', unixStyleFilepath)
   }
 
   const authors = history.nodes.flatMap(({ authors }) => {
