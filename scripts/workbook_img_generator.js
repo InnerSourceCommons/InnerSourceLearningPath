ab8d8ecef2aa536a1651f59e1a9d515adcc020a3
Laura Little <935915+marshmallowrobot@users.noreply.github.com>
2023-01-03 12:30:32 -0500

    changed overlay image to be more like the old green style

--- scripts/workbook_img_generator.js
+++ scripts/workbook_img_generator.js
@@ -5,54 +5,62 @@ const puppeteer = require('puppeteer');
 const htmlHead = () => {
   return `
   <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro&family=PT+Sans&display=swap" rel="stylesheet">
     <style>
+      @import url('https://fonts.googleapis.com/css2?family=Source+Serif+Pro:wght@400;700&family=PT+Sans:wght@400;700&display=swap');
       * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
       }
-      @font-face {
-        font-family: "PT Sans";
-        src: url("https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap");
-      }
       html {
-        font-family: "PT Sans";
-        font-size: 16 px;
-        color: black;
+        font-family: 'PT Sans';
+        font-size: 12px;
+        color: white;
       }
       body {
-        min-width: 1058px;
-        min-height: 595px;
+        min-height: 576px;
+        min-width: 1024px;
       }
-      .overlay-container {
-        background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/A_laptop_and_a_notebook_on_the_wooden_table_%28Pixabay%29.jpg/1024px-A_laptop_and_a_notebook_on_the_wooden_table_%28Pixabay%29.jpg");
+      .workbook-image-container {
+        background-image: url('https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/A_laptop_and_a_notebook_on_the_wooden_table_%28Pixabay%29.jpg/1024px-A_laptop_and_a_notebook_on_the_wooden_table_%28Pixabay%29.jpg');
         background-repeat: no-repeat;
-        padding: 0;
-        margin: 0;
-        height: 595px;
-        width: 1058px;
-      }
-      .logo {
-        padding: 0;
+        display: flex;
+        flex-direction: row;
+        /* maintain 16:9 aspect ratio */
+        height: 576px;
+        width: 1024px;
       }
       .color-field {
-        background-color: #edf6f5;
-        display: flex;
-        flex-direction: column;
-        height: 595px;
-        padding: 24px;
-        width: 540px;
+        border-bottom: 576px solid #8BAB84;
+        border-right: 180px solid transparent;
+        position: relative;
+        width: 512px;
+        z-index: 0;
       }
-      .section {
-        border-bottom: 2px solid #0aa8a7;
-        color: #595959;
-        font-size: 1.8rem;
-        margin-top: 60px;
-        padding-bottom: 8px;
+      .isc-logo {
+        margin: 2rem 0 0 2rem;
+        position: absolute;
+        z-index: 1;
       }
-      .title {
-        font-size: 2.4rem;
-        margin-top: 60px;
+      .isc-logo img {
+        width: 256px;
+      }
+      .section-name {
+        font-family: 'Source Serif Pro', serif;
+        font-size: 5rem;
+        margin: 26rem 0 0 2rem;
+        position: absolute;
+        z-index: 1;
+      }
+      .workbook-title {
+        font-family: 'PT Sans', sans-serif;
+        font-size: 2.2rem;
+        position: absolute;
+        margin: 32rem 0 0 2rem;
+        z-index: 1;
       }
     </style>
   </head>
@@ -62,12 +70,11 @@ const htmlHead = () => {
 const htmlBody = (sectionName, itemName) => {
   return `
   <body>
-    <div class="overlay-container">
-      <div class="color-field">
-        <span class="logo"><img width="200px" src="https://innersourcecommons.org/images/logo.png" alt="InnerSource Commons"></span>
-        <span class="section">${sectionName}</span>
-        <span class="title">${itemName}</span>
-      </div>
+    <div class="workbook-image-container">
+      <div class="color-field"></div>
+      <span class="isc-logo"><img src="https://innersourcecommons.org/images/logo.png" alt="InnerSource Commons"></span>
+      <span class="section-name">${sectionName}</span>
+      <span class="workbook-title">${itemName}</span>
     </div>
   </body>
   `;
@@ -83,7 +90,7 @@ const imageMarkup = (sectionName, itemName) => {
   `;
 }
 
-module.exports = createWorkbookImage = async (writePath, sectionName, itemName) => {
+module.exports = generateWorkbookImage = async (writePath, sectionName, itemName) => {
   const imageName = `${sectionName}-${itemName}-image.png`;
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
